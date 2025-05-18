47d2837ff531e108be19613bbeb357fcbd545149
Niraj-commits <karki.ni018@gmail.com>
2025-05-18 21:18:55 +0545

    added models for the accounts section

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from core.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+
+class Product(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+    cost_price = models.PositiveIntegerField()
+    selling_price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+class Payment(models.Model):
+    payment_methods = [('esewa','esewa'),('khalti','khalti'),('cash','cash')]
+    methods = models.CharField(max_length=15,choices=payment_methods,default="cash")
+
+class POS(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment,on_delete=models.CASCADE)
+
+class GRN(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment,on_delete=models.CASCADE)
+
+class Purchase_order(models.Model):
+    order_status = [('pending','pending'),('completed','completed'),('cancelled','cancelled')]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.CharField(max_length=25,choices=order_status,default="pending")
+
+class Purchase_Order_Item(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    order = models.ForeignKey(Purchase_order,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
