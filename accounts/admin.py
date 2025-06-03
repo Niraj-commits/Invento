008321bb4e2f324377e18978b0ba11ab3cca069e
Niraj-commits <karki.ni018@gmail.com>
2025-06-03 21:17:07 +0545

    added custom admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class category(admin.ModelAdmin):
+    list_display = ['id','name']
+    list_editable = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+
+class product(admin.ModelAdmin):
+    list_display = ['id','name','cost_price','selling_price','category']
+    list_editable = ['name','cost_price','selling_price']
+    list_filter = ['category']
+    search_fields = ['name','category']
+
+
+admin.site.register(Category,category)
+admin.site.register(Product,product)
 admin.site.register(POS)
 admin.site.register(POS_Item)
 admin.site.register(GRN)
