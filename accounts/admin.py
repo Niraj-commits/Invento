7cb0525bb48d54e799b82e66887f1a1be52bdef2
Niraj-commits <karki.ni018@gmail.com>
2025-06-03 21:11:54 +0545

    updated admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(POS)
+admin.site.register(POS_Item)
+admin.site.register(GRN)
+admin.site.register(Purchase_order)
+admin.site.register(Purchase_Order_Item)
