3fa5d19709faea44e4e3c13cee9951acc919f97a
Niraj-commits <karki.ni018@gmail.com>
2025-07-02 10:21:50 +0545

    added serializer for category

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -7,11 +7,48 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id','name']
 
+    def create(self, validated_data):
+        occurence = Category.objects.filter(name = validated_data.get('name')).exists()
+        if occurence:
+            raise serializers.ValidationError({"detail":"categories cannot have same name"})
+        category = self.Meta.model(**validated_data) #as the condition is true creatin new data
+        category.save()
+        return category
+    
+    def update(self,instance,validated_data):
+
+        new_name = validated_data.get('name')
+
+        if instance.name != new_name: #checking if name is changed
+            duplicate = Category.objects.filter(name = new_name).exclude(pk = instance.pk).exists()
+            if duplicate:
+                raise serializers.ValidationError({"detail":"Category with that name already exists"})
+    
+        instance.name = validated_data.get('name')
+        instance.save()
+        return instance
+    
+        # OR for multiple values
+        # for attr,values in validated_data.items():
+        #     setattr(instance,attr,values)
+        # instance.save()
+        # return instance
+
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
         fields = ['id','name','description','cost_price','selling_price','category']
+
+    def create(self,validated_data):
+        duplicate = Product.objects.filter(name = validated_data.get('name'),category = validated_data.get('category')).exists()
+
+        if duplicate:
+            raise serializers.ValidationError({"Detail":"Duplicate Products in same category"})
+        
+        product = self.Meta.model(**validated_data)
+        product.save()
+        return product
     
 class PaymentSerializer(serializers.ModelSerializer):
 
